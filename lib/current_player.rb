542fd60c4132c8248d7731e2350b1bb782830280
md mia <sharifmia091@gmail.com>
2018-08-19 15:57:11 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   counter = 0
 
   board.each do |character|
-    if character=="X" || character=="0"
+    if (character=="X" || character=="0")
       counter += 1
     end
   end
